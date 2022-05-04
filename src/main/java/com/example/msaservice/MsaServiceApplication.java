f836b6ea9f9484b70b0f552fc4d37f0ca0fcf0d5
DeokhunKim <86358502+DeokhunKim@users.noreply.github.com>
2022-05-04 15:51:39 +0900

    Add basic spring boot

---
+++ src/main/java/com/example/msaservice/MsaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaServiceApplication.class, args);
+	}
+
+}
